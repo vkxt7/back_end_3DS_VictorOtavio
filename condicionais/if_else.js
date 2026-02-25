24811b8061cccbf9f327014b0f737e8fc741e31c
vkxt7 <victor.pietzuk@escola.pr.gov.br>
2026-02-25 11:35:44 +0000

    else e sistema de login

---
+++ condicionais/if_else.js
@@ -0,0 +1,14 @@
+// if, else, else if, ternário
+// Serve para tomada de decisão (se/senão)
+
+let idadePessoas = 100
+
+if (idadePessoas > 100 ) {
+    console.log ("Rala bixo fei")
+
+} else if (idadePessoas >= 17 && idadePessoas <= 100 ) {
+    console.log ("Bem vindo, a festa é sua!!")
+}
+   else {
+    console.log ("Não é festa para menores ")
+   }
---
+++ condicionais/if_login.js
@@ -0,0 +1,18 @@
+let login = "enzo543"
+let senha = 12345
+
+if (login == "enzo543") {
+    console.log  ("Login correto")
+    
+} else {
+    console.log ("Login incorreto")
+    
+}
+
+if (senha == "12345") {
+    console.log  ("Senha correta !")
+    
+} else {
+    console.log ("Senha incorreta ")
+    
+}
