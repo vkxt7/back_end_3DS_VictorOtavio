2092c49786e6810bec103bb79c8d4b3abc75a0e8
vkxt7 <victor.pietzuk@escola.pr.gov.br>
2026-03-04 11:47:06 +0000

    arrey-time

---
+++ arreys/arrey-time.js
@@ -0,0 +1,16 @@
+// Grupo de valores indexados a partir 
+// do ZERO até o infinito
+// possui metodos nativos
+
+const time = ["Julios" , "Chis"]
+// indices       0
+time [5]
+time [6] = "Victor" // atribuição dinamicos
+
+for (const jogador of time) {
+    console.log (jogador )
+    
+}
+
+console.log (time)
+console.log (time[1])
--- condicionais/if_login.js
+++ condicionais/if_login.js
@@ -1,18 +1,10 @@
 let login = "enzo543"
 let senha = 12345
 
-if (login == "enzo543") {
+if (login == "enzo543" && senha == 12345) {
     console.log  ("Login correto")
     
 } else {
-    console.log ("Login incorreto")
-    
-}
-
-if (senha == "12345") {
-    console.log  ("Senha correta !")
-    
-} else {
-    console.log ("Senha incorreta ")
+    console.log ("Erro, favor digitar dados corretamente")
     
 }
