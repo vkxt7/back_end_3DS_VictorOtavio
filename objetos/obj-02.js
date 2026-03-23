35bbd8d13899539198986a201a1e21397382df97
vkxt7 <victor.pietzuk@escola.pr.gov.br>
2026-03-23 12:13:19 +0000

    finalização do obj 02

---
+++ objetos/obj-02.js
@@ -0,0 +1,26 @@
+// Crie um objeto do tema que escolher 
+// Crie pelo menos 5 prioridades
+// Faça uma impressão para Humanos!
+// ex. console.log("Nome do jogador: " + jogador.nome)
+
+const paises = {
+    nome: "Brasil", 
+    capital: "Brasilia",
+    cores: ["Amarelo, Verde, Azul, Branco"],
+    pib: ["Em 2025, o PIB do Brasil atingiu R$ 12,7 trilhões"],
+    idh: ["O IDH (2023, divulgado em 2025) é de 0,786"]
+}
+paises["Nome"] = "pais: Brasil"
+paises.capital = "Capital atual: Brasilia"
+paises.priemiracapital = "Salvador"
+paises.cores = "Cores: Amarelo, Verde, Azul, Branco"
+paises.pib = "PIB: Em 2025, o PIB do Brasil atingiu R$ 12,7 trilhões"
+console.idh = "ID: O IDH (2023, divulgado em 2025) é de 0,786"
+console.log (paises.nome)
+console.log(paises.capital)
+console.log (paises.cores)
+console.log (paises.pib)
+console.log (paises.idh)
+
+
+console.log ("Priemreia capital: " + paises.priemiracapital)
