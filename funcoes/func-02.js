f361f63afcf0d513fc37db378b5ea23d36bb03e5
vkxt7 <victor.pietzuk@escola.pr.gov.br>
2026-03-30 10:55:51 +0000

    Funções atividade

---
+++ funcoes/func-02.js
@@ -0,0 +1,33 @@
+// Crie 4 funções para operações (+, -, * e /)
+// Calcula e imrpime o resultado
+
+function subtracao(n1, n2) {
+    const resultado = n1 - n2
+    console.log ("Resultado da subtração: " + resultado )
+}
+
+
+// Inovação
+subtracao (10, 5)
+
+function adicao(n1, n2) {
+    const resultado = n1 + n2
+    console.log ("Resultado da adição: " + resultado )
+}
+
+adicao(69, 67)
+
+function divisao (n1, n2) { 
+    const resultado = n1 / n2
+    console.log ("Resultado da Divisão: " + resultado )
+}
+
+divisao(4, 2)
+divisao(4, 0)
+
+function multiplicacao (n1, n2) {
+    const resultado = n1 * n2
+    console.log ("Resultado da multiplicação: " + resultado )
+}
+
+multiplicacao(2, 4)
