737a970ec00aac34a766ec4e50c3b2037d0676b6
vkxt7 <victor.pietzuk@escola.pr.gov.br>
2026-05-27 11:49:12 +0000

    finalização do modulo FS com path (join e resolve)

--- 2º trimestre/modulos/mod03-FS/fs-com-path.js
+++ 2º trimestre/modulos/mod03-FS/fs-com-path.js
@@ -1,5 +1,12 @@
 // importar PATH E FS
+// path possiu (Join para juntar caminhos e Resolve
+// para deduzir subdiretórios e S.O.)
+const fs = require('fs')
+const path = require ('path')
 
 // Salvar o Caminho em um variavel
+// const caminho = path.join(__dirname, 'README.md')// Markdown, text
+const caminho = path.resolve(__dirname, 'README.md')// Abrir, ler, deletar, editar
+const arquivo = fs.readFileSync(caminho, 'utf-8')
 
-// Abrir, ler, deletar, editar
+console.log(arquivo)
