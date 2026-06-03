69b84931ae07f69a5e4eeff331c60b2ade80d561
vkxt7 <victor.pietzuk@escola.pr.gov.br>
2026-06-03 12:31:20 +0000

    Arrumar erros

--- 2º trimestre/modulos/mod03-FS/fs-com-path.js
+++ 2º trimestre/modulos/mod03-FS/fs-com-path.js
@@ -2,11 +2,21 @@
 // path possiu (Join para juntar caminhos e Resolve
 // para deduzir subdiretórios e S.O.)
 const fs = require('fs')
-const path = require ('path')
+const path = require('path')
 
 // Salvar o Caminho em um variavel
 // const caminho = path.join(__dirname, 'README.md')// Markdown, text
-const caminho = path.resolve(__dirname, 'README.md')// Abrir, ler, deletar, editar
-const arquivo = fs.readFileSync(caminho, 'utf-8')
+try {
 
-console.log(arquivo)
+    const caminho = path.resolve(__dirname, 'README.md')// Abrir, ler, deletar, editar
+    const arquivo = fs.readFileSync(caminho, 'utf-8')
+
+    console.log(arquivo)
+} catch (erro) {
+
+   console.log('----------------------------')
+   console.warn ("Atenção!"+erro.path)
+    throw new Error("Algo deu errado na sua operação!");
+    console.log('---------------------------')
+}
+
